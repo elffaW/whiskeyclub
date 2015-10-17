8241ad2616da87e3bdf6135a4110122eea9fa125
Mike <mwilli26@gmail.com>
2015-10-16 23:54:15 -0400

    fix whiskey stats updating, change survey layout

--- js/whiskeyform.js
+++ js/whiskeyform.js
@@ -5,7 +5,7 @@ var UserSection = React.createClass({
     render() {
         var usernames = this.props.users.map(function(user) {
             return (
-                <option value={user.username}>{user.username}</option>
+                <option value={user.username} key={user.userId}>{user.username}</option>
             );
         });
         return (
@@ -28,42 +28,73 @@ var UserSection = React.createClass({
     }
 });
 
+/* Sample whiskey JSON
+     "whiskeyId":1,
+     "name":"Buffalo Trace",
+     "whiskeyType":"Kentucky Straight Bourbon",
+     "proof":90,
+     "wood":"Charred white oak",
+     "age":2
+ */
 var WhiskeySection = React.createClass({
+    handleChange() {
+        this.props.onWhiskeySelect(
+            this.refs.whiskeySelector.getInputDOMNode().value
+        );
+    },
 
     render() {
         var whiskeynames = this.props.whiskeys.map(function(whiskey) {
             return (
-                <option value={whiskey.name}>{whiskey.name}</option>
+                <option value={whiskey.whiskeyId} key={whiskey.whiskeyId}>{whiskey.name}</option>
             );
         });
+
+        var whiskeyType = '',
+            whiskeyWood = '',
+            whiskeyABV = '',
+            whiskeyAge = '',
+            whiskeyProof = '';
+
+        this.props.whiskeys.map(function(whiskey) {
+            if(whiskey.whiskeyId == this.props.whiskeyId) {
+                whiskeyType = whiskey.whiskeyType;
+                whiskeyWood = whiskey.wood;
+                whiskeyABV = whiskey.proof/2;
+                whiskeyAge = whiskey.age + " years";
+                whiskeyProof = whiskey.proof;
+
+            }
+        }.bind(this));
+
         return (
             <ReactBootstrap.Panel collapsible defaultExpanded header={<strong>Whiskey Details</strong>} >
 
                 <ReactBootstrap.Grid fluid>
                     <ReactBootstrap.Row>
                         <ReactBootstrap.Col xs={12} sm={6} >
-                            <ReactBootstrap.Input type="select" label="Name" labelClassName="col-xs-2" wrapperClassName="col-xs-10" >
+                            <ReactBootstrap.Input type="select" label="Name" ref="whiskeySelector" onChange={this.handleChange} labelClassName="col-xs-2" wrapperClassName="col-xs-10" >
                                 {whiskeynames}
                             </ReactBootstrap.Input>
                         </ReactBootstrap.Col>
                         <ReactBootstrap.Col xs={12} sm={6} >
-                            <ReactBootstrap.Input type="text" label="Type" readOnly labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
+                            <ReactBootstrap.FormControls.Static label="Type" value={whiskeyType} labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
                         </ReactBootstrap.Col>
                     </ReactBootstrap.Row>
                     <ReactBootstrap.Row>
                         <ReactBootstrap.Col xs={12} sm={6} >
-                            <ReactBootstrap.Input type="text" label="Wood" readOnly labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
+                            <ReactBootstrap.FormControls.Static label="Wood" value={whiskeyWood} labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
                         </ReactBootstrap.Col>
                         <ReactBootstrap.Col xs={12} sm={6} >
-                            <ReactBootstrap.Input type="number" label="ABV" max="100" min="0" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
+                            <ReactBootstrap.FormControls.Static label="ABV" value={whiskeyABV} labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
                         </ReactBootstrap.Col>
                     </ReactBootstrap.Row>
                     <ReactBootstrap.Row>
                         <ReactBootstrap.Col xs={12} sm={6} >
-                            <ReactBootstrap.Input type="text" label="Age" readOnly labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
+                            <ReactBootstrap.FormControls.Static label="Age" value={whiskeyAge} labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
                         </ReactBootstrap.Col>
                         <ReactBootstrap.Col xs={12} sm={6} >
-                            <ReactBootstrap.FormControls.Static label="Proof" value="90#" readOnly labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
+                            <ReactBootstrap.FormControls.Static label="Proof" value={whiskeyProof} labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
                         </ReactBootstrap.Col>
                     </ReactBootstrap.Row>
 
@@ -82,92 +113,104 @@ var SurveySection = React.createClass({
             <ReactBootstrap.Panel collapsible defaultExpanded header={<strong>Whiskey Ratings</strong>} >
 
                 <ReactBootstrap.Grid fluid>
+                    <ReactBootstrap.Well className="borderWell">
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="textarea"
+                                                  label="Appearance"
+                                                  help="Color? Cloudy/Hazy? Sediment?"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="number"
+                                                  label="Ranking"
+                                                  min="1"
+                                                  max="10"
+                                                  placeholder="1-10"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                    </ReactBootstrap.Well>
+
+                    <ReactBootstrap.Well className="borderWell">
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="textarea"
+                                                  label="Aroma"
+                                                  help="Examples: Boozy Sweet Caramel Chocolate Tobacco Musty Phenolic Maple Syrup"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="number"
+                                                  label="Ranking"
+                                                  min="1"
+                                                  max="10"
+                                                  placeholder="1-10"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                    </ReactBootstrap.Well>
+
+                    <ReactBootstrap.Well className="borderWell">
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="textarea"
+                                                  label="Taste"
+                                                  placeholder="Try adding ice if it tastes too boozy!"
+                                                  help="Examples: Boozy Woody Chocolate Tobacco Green/Phenolic Sweet Maple-Syrup Smoky Musty Caramel Honey/Molasses/Brown-Sugar Toasted Dirty Salty"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="textarea"
+                                                  label="Mouthfeel"
+                                                  help="Examples: Smooth Harsh Oily Gritty Thick/Thin"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="number"
+                                                  label="Ranking"
+                                                  min="1"
+                                                  max="10"
+                                                  placeholder="1-10"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                    </ReactBootstrap.Well>
 
                     <ReactBootstrap.Row>
-                        <ReactBootstrap.Input type="textarea" label="Appearance" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-                    </ReactBootstrap.Row>
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Col xs={12} xsOffset={2} >
-                            <ReactBootstrap.ButtonGroup>
-                                <ReactBootstrap.Button>1</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>2</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>3</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>4</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>5</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>6</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>7</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>8</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>9</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>10</ReactBootstrap.Button>
-                            </ReactBootstrap.ButtonGroup><br/><br/>
-                        </ReactBootstrap.Col>
+                        <ReactBootstrap.FormControls.Static label="Total Score" value='0-30' labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
                     </ReactBootstrap.Row>
 
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Input type="textarea" label="Aroma" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-                    </ReactBootstrap.Row>
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Col xs={12} xsOffset={2} >
-                            <ReactBootstrap.ButtonGroup>
-                                <ReactBootstrap.Button>1</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>2</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>3</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>4</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>5</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>6</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>7</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>8</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>9</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>10</ReactBootstrap.Button>
-                            </ReactBootstrap.ButtonGroup> <br/><br/>
-                        </ReactBootstrap.Col>
+                    <ReactBootstrap.Well className="borderWell">
+                        <ReactBootstrap.Row>
+                        <ReactBootstrap.Input type="textarea"
+                                              label="Recommended?"
+                                              addonBefore={innerCheckbox}
+                                              placeholder="What setting?"
+                                              labelClassName="col-xs-2"
+                                              wrapperClassName="col-xs-10" />
                     </ReactBootstrap.Row>
 
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="textarea"
+                                                  label="Bottle Design"
+                                                  help="Label? Bottle? Cap? Colors? Name?"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                        <ReactBootstrap.Row>
+                            <ReactBootstrap.Input type="number"
+                                                  label="Ranking"
+                                                  min="1"
+                                                  max="10"
+                                                  placeholder="1-10"
+                                                  labelClassName="col-xs-2"
+                                                  wrapperClassName="col-xs-10" />
+                        </ReactBootstrap.Row>
+                    </ReactBootstrap.Well>
                     <ReactBootstrap.Row>
-                        <ReactBootstrap.Input type="textarea" label="Taste" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-                    </ReactBootstrap.Row>
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Input type="textarea" label="Mouthfeel" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-                    </ReactBootstrap.Row>
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Col xs={12} xsOffset={2} >
-                            <ReactBootstrap.ButtonGroup>
-                                <ReactBootstrap.Button>1</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>2</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>3</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>4</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>5</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>6</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>7</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>8</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>9</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>10</ReactBootstrap.Button>
-                            </ReactBootstrap.ButtonGroup><br/><br/>
-                        </ReactBootstrap.Col>
-                    </ReactBootstrap.Row>
-
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Input type="textarea" label="Recommended?" addonBefore={innerCheckbox} placeholder="What setting?" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-                    </ReactBootstrap.Row>
-
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Input type="textarea" label="Bottle Design" labelClassName="col-xs-2" wrapperClassName="col-xs-10" />
-                    </ReactBootstrap.Row>
-                    <ReactBootstrap.Row>
-                        <ReactBootstrap.Col xs={12} xsOffset={2} >
-                            <ReactBootstrap.ButtonGroup>
-                                <ReactBootstrap.Button>1</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>2</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>3</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>4</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>5</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>6</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>7</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>8</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>9</ReactBootstrap.Button>
-                                <ReactBootstrap.Button>10</ReactBootstrap.Button>
-                            </ReactBootstrap.ButtonGroup><br/><br/>
-                        </ReactBootstrap.Col>
+                        <ReactBootstrap.ButtonInput type="submit" value="Submit" style={{display:'block',marginLeft:'auto',marginRight:'auto'}} />
                     </ReactBootstrap.Row>
                 </ReactBootstrap.Grid>
 
@@ -177,6 +220,17 @@ var SurveySection = React.createClass({
 });
 
 var WhiskeyForm = React.createClass({
+    getInitialState() {
+        return {
+            whiskeyId:1
+        };
+    },
+
+    handleWhiskeySelect(whiskeyId) {
+        this.setState({
+            whiskeyId: whiskeyId
+        });
+    },
 
     render() {
         return (
@@ -184,7 +238,9 @@ var WhiskeyForm = React.createClass({
                 <form className="form-horizontal">
                     <UserSection users={this.props.users} />
 
-                    <WhiskeySection whiskeys={this.props.whiskeys} />
+                    <WhiskeySection whiskeys={this.props.whiskeys}
+                                    whiskeyId={this.state.whiskeyId}
+                                    onWhiskeySelect={this.handleWhiskeySelect} />
 
                     <SurveySection />
 
@@ -220,7 +276,7 @@ var WHISKEYS=[
     {
         "whiskeyId":1,
         "name":"Buffalo Trace",
-        "type":"Kentucky Straight Bourbon",
+        "whiskeyType":"Kentucky Straight Bourbon",
         "proof":90,
         "wood":"Charred white oak",
         "age":2
@@ -228,7 +284,7 @@ var WHISKEYS=[
     {
         "whiskeyId":2,
         "name":"Old Grand Dad",
-        "type":"Kentucky Straight Bourbon",
+        "whiskeyType":"Kentucky Straight Bourbon",
         "proof":80,
         "wood":"Charred white oak",
         "age":2
